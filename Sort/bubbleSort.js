2f3630d32a10b355ec655acc9f7c5e9a0441a8f4
Hamed Hafeznia <hamedhafeznia@Hameds-MacBook-Pro.local>
2022-01-25 21:31:49 +0330

    Implement bubble sort

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(array) {
+    for(let i = array.length; i > 0; i--) {
+        for(let j = 0; j < i - 1; j++) {
+            // console.log(array[j], array[j + 1])
+            if(array[j] > array[j + 1]) {
+                let temp = array[j]
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(bubbleSort([3,2,7,4,1,8]))
