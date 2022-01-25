f6f1dc5edc913e7e24694fcfd5489d47a7c0fc1b
Hamed Hafeznia <hamedhafeznia@Hameds-MacBook-Pro.local>
2022-01-25 21:54:30 +0330

    add no swaps

--- Sort/bubbleSort.js
+++ Sort/bubbleSort.js
@@ -1,13 +1,17 @@
 function bubbleSort(array) {
+    let noSwaps;
     for(let i = array.length; i > 0; i--) {
+        noSwaps = true;
         for(let j = 0; j < i - 1; j++) {
             // console.log(array[j], array[j + 1])
             if(array[j] > array[j + 1]) {
                 let temp = array[j]
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                noSwaps = false;
             }
         }
+        if(noSwaps) break;
     }
     return array;
 }
