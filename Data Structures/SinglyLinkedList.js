c16fbd2b3ec88b8c4f28a01dbf73519240387cd9
Hamed Hafeznia <hafezniyaa@gmail.com>
2022-02-06 23:45:50 +0330

    Add reverse to singly linked list

--- Data Structures/SinglyLinkedList.js
+++ Data Structures/SinglyLinkedList.js
@@ -101,6 +101,34 @@ class SinglyLinkedList {
         this.length++;
         return true;
     }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return false;
+        if(index === this.length - 1) return this.pop();
+        if(index === 0) return this.shift(val);
+        var previousNode = this.get(index - 1);
+        var removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+        for(var i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+        
+    }
+
 }
 
 var list = new SinglyLinkedList();
