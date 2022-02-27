45a0d981148f5dad670cfbe4a976cca016c5a07b
Hamed Hafeznia <hafezniyaa@gmail.com>
2022-02-27 16:09:43 +0330

    Implement BST

---
+++ Data Structures/BinarySearchTree.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(val) {
+      this.value = val;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  class BinarySearchTree {
+      constructor() {
+          this.root = null
+      }
+
+      insert(val) {
+        var newNode = new Node(val);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        else {
+          var current = this.root;
+          while(true) {
+              if(val === current.val) return undefined;
+              if(val < current.value) {
+                  if(current.left === null) {
+                      current.left = newNode;
+                      return this;
+                  } else {
+                      current = current.left;
+                  }
+              } else if(val > current.value) {
+                  if(current.right === null) {
+                      current.right = newNode;
+                      return this;
+                  } else {
+                      current = current.right
+                  }
+              }
+          }
+        }
+      }
+  }
+
+var tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(6)
