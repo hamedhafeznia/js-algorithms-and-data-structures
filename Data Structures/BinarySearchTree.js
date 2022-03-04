aeb220f49d8831f663bbad72f40d1a802b484b5f
Hamed Hafeznia <hafezniyaa@gmail.com>
2022-03-05 03:04:11 +0330

    BST is done

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -39,6 +39,27 @@ class Node {
           }
         }
       }
+
+      find(val) {
+          if(this.root === null) return false
+
+          var current = this.root,
+              found = false;
+        
+        while (current && !found) {
+            if(val < current.value) {
+                current = current.left
+            } else if(val > current.value) {
+                current = current.right
+            } else {
+                found = true       
+            }
+        }
+        if(!found) return false;
+        return current;
+
+      }
+
   }
 
 var tree = new BinarySearchTree()
